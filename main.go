b2435c11354a27a0bfb541be79145a6fd3bcb9be
Jack McConnell <jackmc1122@gmail.com>
2019-10-13 17:24:37 -0700

    Finish chat server tutorial

--- main.go
+++ main.go
@@ -23,7 +23,12 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	flag.Parse()
+	hub := newHub()
+	go hub.run()
 	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
